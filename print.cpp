8615d54e09f0f18898ebf9e768a06fdd8030aee4
asraful-07 <asraful0508@gmail.com>
2025-10-18 11:59:32 +0600

    add c++ number print

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    int x = 10;
+    char c = 'A';
+    double  d = 36.94;
+
+    std :: cout << x << " " << c << std::endl << d << std::endl; 
+    std :: cout << "My favorite number is " << x << std::endl; 
+    return 0;
+}
